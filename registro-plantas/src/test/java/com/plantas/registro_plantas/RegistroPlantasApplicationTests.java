df5ed0031b990e6dc8ff08cff949ee803d31a6fd
Emerson Huaman <emerson.huaman1@unmsm.edu.pe>
2025-07-28 19:36:09 -0500

    Se agrega el proyecto

---
+++ registro-plantas/src/test/java/com/plantas/registro_plantas/RegistroPlantasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantas.registro_plantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroPlantasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
